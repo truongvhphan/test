7175f1ddd64860352c841df8dcd9068b662b247d
truongphan <truongphv@gmail.com>
2018-05-22 14:52:12 +0700

    Thay doi app_controller.php

--- Controllers/app_controller.php
+++ Controllers/app_controller.php
@@ -8,10 +8,12 @@ class AppController{
             $pathArr = explode('/',$path);
             $path = $pathArr[count($pathArr)-1];
             $pathArr = explode('_', $path);
-            $path = $pathArr[0];
             
-            if($_SERVER['QUERY_STRING']){
+            
+            if(isset($_SERVER['QUERY_STRING'])){
+                
                 $query_string = $_SERVER['QUERY_STRING'];
+                
                 if(strpos($query_string, '&')){
                     $query_stringArr = explode('&', $query_string);
                     $query_stringArr = $query_stringArr[0];
@@ -20,9 +22,10 @@ class AppController{
                 {
                     $query_stringArr = $query_string;
                 }
-                if(strpos($query_stringArr, 'action')){
+                if(strpos($query_stringArr, 'action') > -1){
                     $pattern = array('/action/', '/=/');
                     $query_string = trim(preg_replace($pattern,'',$query_stringArr));
+                    
                 }
                 else{
                     $query_string = 'index';
@@ -32,6 +35,7 @@ class AppController{
             {
                 $query_string = 'index';
             }
+            $path = $pathArr[0];
         }
         else
         {
@@ -42,6 +46,7 @@ class AppController{
             $path = $pathArr[0];
             $query_string = $menu;
         }
+        
         return '../views/' . $path . '/' . $query_string . '.php';
     }
        
